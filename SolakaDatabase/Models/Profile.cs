055027017ad5761234af65c7bb4b12ade18d658d
Haekal Akmal Tsaqif <akmaltsaqif70@gmail.com>
2022-05-23 16:49:32 +0700

    Add User Service

---
+++ SolakaDatabase/Models/Profile.cs
@@ -0,0 +1,10 @@
+﻿namespace SolakaDatabase.Models
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+        public string Fullname { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string RoleName { get; set; }
+    }
+}
