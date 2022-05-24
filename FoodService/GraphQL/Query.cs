0521fcb5659407ce66c7d2839b85dc9a9ec54095
Ndaru96 <ndarupenangsang7@gmail.com>
2022-05-24 15:06:00 +0700

    fixed login token

--- FoodService/GraphQL/Query.cs
+++ FoodService/GraphQL/Query.cs
@@ -7,12 +7,12 @@ namespace FoodService.GraphQL
     public class Query
     {
         [Authorize] // dapat diakses kalau sudah login
-        public IQueryable<Product> GetFood([Service] SolakaDbContext context, ClaimsPrincipal claimsPrincipal)
+        public IQueryable<Product> GetFoods([Service] SolakaDbContext context, ClaimsPrincipal claimsPrincipal)
         {
             var userName = claimsPrincipal.Identity.Name;
 
             // check admin role ?
-            var adminRole = claimsPrincipal.Claims.Where(o => o.Type == ClaimTypes.Role).FirstOrDefault();
+            var adminRole = claimsPrincipal.Claims.Where(p => p.Type == ClaimTypes.Role).FirstOrDefault();
             var user = context.Users.Where(o => o.Username == userName).FirstOrDefault();
             if (user != null)
             {
