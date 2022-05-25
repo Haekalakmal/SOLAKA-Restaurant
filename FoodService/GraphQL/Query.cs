48b60da16bb4bd9bcb650c69adcdcc26865b455c
Ndaru96 <ndarupenangsang7@gmail.com>
2022-05-26 02:04:16 +0700

    fixed query food

--- FoodService/GraphQL/Query.cs
+++ FoodService/GraphQL/Query.cs
@@ -16,7 +16,7 @@ namespace FoodService.GraphQL
             var user = context.Users.Where(o => o.Username == userName).FirstOrDefault();
             if (user != null)
             {
-                if (adminRole.Value == "Customer" || adminRole.Value == "ManagerResto")
+                if (adminRole.Value == "Customer")
                 {
                     return context.Products;
                 }
@@ -25,6 +25,22 @@ namespace FoodService.GraphQL
             }
 
 
+            return new List<Product>().AsQueryable();
+        }
+
+        [Authorize(Roles = new[] { "ManagerResto" })]
+        public IQueryable<Product> GetFoodsByManagerResto([Service] SolakaDbContext context, ClaimsPrincipal claimsPrincipal)
+        {
+            var userName = claimsPrincipal.Identity.Name;
+            var user = context.Users.Where(o => o.Username == userName).FirstOrDefault();
+            //var resto = context.Restaurants.FirstOrDefault();
+            var managerResto = context.EmployeeRestos.Where(e => e.UserId == user.Id).FirstOrDefault();
+
+            if (managerResto != null)
+            {
+                var food = context.Products.Where(o => o.RestoId == managerResto.RestoId);
+                return food.AsQueryable();
+            }
             return new List<Product>().AsQueryable();
         }
     }
