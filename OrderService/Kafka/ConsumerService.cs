a4490eceac3b294b5cf9878e26f9568caf9bcb5c
rezzabadruzzaman54 <rezzabadruzzaman54@gmail.com>
2022-05-27 04:14:55 +0700

    Menambahkan Background Consumer Service pada OrderService

---
+++ OrderService/Kafka/ConsumerService.cs
@@ -0,0 +1,75 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+using OrderService.GraphQL;
+using SolakaDatabase.Models;
+
+namespace OrderService.Kafka
+{
+    public class ConsumerService : BackgroundService
+    {
+        private readonly IConfiguration config;
+        private readonly ConsumerConfig _consumerConfig;
+
+        public ConsumerService(IConfiguration configuration)
+        {
+            config = configuration;
+            _consumerConfig = new ConsumerConfig
+            {
+                BootstrapServers = config.GetSection("KafkaSettings").GetValue<string>("Server"),
+                GroupId = "tester",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Console.WriteLine("!!! CONSUMER STARTED !!!\n");
+
+            var task = Task.Run(() => ProcessQueue(stoppingToken), stoppingToken);
+
+            return task;
+        }
+
+        private void ProcessQueue(CancellationToken stoppingToken)
+        {
+            var topic = "SOLAKA";
+
+            using (var consumer = new ConsumerBuilder<string, string>(_consumerConfig).Build())
+            {
+                Console.WriteLine("Connected");
+                consumer.Subscribe(topic);
+                try
+                {
+                    while (!stoppingToken.IsCancellationRequested)
+                    {
+                        var cr = consumer.Consume(stoppingToken);
+                        Console.WriteLine($"Consumed record with key: {cr.Message.Key} and value: {cr.Message.Value}");
+
+                        using (var context = new SolakaDbContext())
+                        {
+                            var val2 = JsonConvert.DeserializeObject<SendDataOrder>(cr.Message.Value);
+
+                            var order = context.Orders.Where(o => o.Id == val2.TransactionId).FirstOrDefault();
+                            //var order = context.Orders.Include(o=>o.OrderDetails).Where(o=>o.Id==val2.TransactionId).FirstOrDefault();
+
+                            order.Status = val2.PaymentStatus;
+
+                            context.Orders.Update(order);
+                            context.SaveChanges();
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    consumer.Close();
+                    // Ctrl-C was pressed.
+                }
+                finally
+                {
+                    consumer.Close();
+                }
+            }
+
+        }
+    }
+}
--- OrderService/Program.cs
+++ OrderService/Program.cs
@@ -45,6 +45,9 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 //inject Kafka
 builder.Services.Configure<KafkaSettings>(builder.Configuration.GetSection("KafkaSettings"));
 
+//Consumer Service (Background Service)
+builder.Services.AddHostedService<ConsumerService>();
+
 var app = builder.Build();
 
 app.UseAuthentication();
@@ -54,70 +57,70 @@ app.MapGet("/", () => "Hello World!");
 
 app.Run();
 
-//===========================KAFKA======================================
-var config = new ConsumerConfig
-{
-    BootstrapServers = "127.0.0.1:9092",
-    GroupId = "tester",
-    AutoOffsetReset = AutoOffsetReset.Earliest
-};
+////===========================KAFKA======================================
+//var config = new ConsumerConfig
+//{
+//    BootstrapServers = "127.0.0.1:9092",
+//    GroupId = "tester",
+//    AutoOffsetReset = AutoOffsetReset.Earliest
+//};
 
-var topic = "SOLAKA";
-CancellationTokenSource cts = new CancellationTokenSource();
-Console.CancelKeyPress += (_, e) => {
-    e.Cancel = true; // prevent the process from terminating.
-    cts.Cancel();
-};
+//var topic = "SOLAKA";
+//CancellationTokenSource cts = new CancellationTokenSource();
+//Console.CancelKeyPress += (_, e) => {
+//    e.Cancel = true; // prevent the process from terminating.
+//    cts.Cancel();
+//};
 
-using (var consumer = new ConsumerBuilder<string, string>(config).Build())
-{
-    Console.WriteLine("Connected");
-    consumer.Subscribe(topic);
-    try
-    {
-        while (true)
-        {
-            var cr = consumer.Consume(cts.Token);
-            Console.WriteLine($"Consumed record with key: {cr.Message.Key} and value: {cr.Message.Value}");
+//using (var consumer = new ConsumerBuilder<string, string>(config).Build())
+//{
+//    Console.WriteLine("Connected");
+//    consumer.Subscribe(topic);
+//    try
+//    {
+//        while (true)
+//        {
+//            var cr = consumer.Consume(cts.Token);
+//            Console.WriteLine($"Consumed record with key: {cr.Message.Key} and value: {cr.Message.Value}");
 
-            using (var context = new SolakaDbContext())
-            {
-                var val2 = JsonConvert.DeserializeObject<SendDataOrder>(cr.Message.Value);
+//            using (var context = new SolakaDbContext())
+//            {
+//                var val2 = JsonConvert.DeserializeObject<SendDataOrder>(cr.Message.Value);
 
-                var order = context.Orders.Where(o => o.Id == val2.TransactionId).FirstOrDefault();
-                //var order = context.Orders.Include(o=>o.OrderDetails).Where(o=>o.Id==val2.TransactionId).FirstOrDefault();
+//                var order = context.Orders.Where(o => o.Id == val2.TransactionId).FirstOrDefault();
+//                //var order = context.Orders.Include(o=>o.OrderDetails).Where(o=>o.Id==val2.TransactionId).FirstOrDefault();
                
-                order.Status = val2.PaymentStatus;
+//                order.Status = val2.PaymentStatus;
 
-                //if (val2.PaymentStatus == "Completed")
-                //{
-                //    foreach (var item in order.OrderDetails)
-                //    {
-                //        var product = context.Products.Where(p => p.Id == item.ProductId).FirstOrDefault();
-                //        //if (product.Stock <= item.Quantity) continue; 
-                //        if (product.Stock < item.Quantity)
-                //        {
-                //            order.Status = "Gagal";
-                //            break;
-                //        }
+//                //if (val2.PaymentStatus == "Completed")
+//                //{
+//                //    foreach (var item in order.OrderDetails)
+//                //    {
+//                //        var product = context.Products.Where(p => p.Id == item.ProductId).FirstOrDefault();
+//                //        //if (product.Stock <= item.Quantity) continue; 
+//                //        if (product.Stock < item.Quantity)
+//                //        {
+//                //            order.Status = "Gagal";
+//                //            break;
+//                //        }
 
-                //        product.Stock -= item.Quantity;
-                //    }
-                //}
+//                //        product.Stock -= item.Quantity;
+//                //    }
+//                //}
 
-                context.Orders.Update(order);
-                await context.SaveChangesAsync();
+//                context.Orders.Update(order);
+//                await context.SaveChangesAsync();
 
-            }
-        }
-    }
-    catch (OperationCanceledException)
-    {
-        // Ctrl-C was pressed.
-    }
-    finally
-    {
-        consumer.Close();
-    }
-}
+//            }
+//        }
+//    }
+//    catch (OperationCanceledException)
+//    {
+//        // Ctrl-C was pressed.
+//    }
+//    finally
+//    {
+//        consumer.Close();
+//    }
+//}
 //=================================================================
